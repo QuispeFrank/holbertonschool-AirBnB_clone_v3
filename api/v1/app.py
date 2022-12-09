a34c001d8d52e2790059f1a684501708bdae2a47
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-08 19:36:19 -0500

    fix file app.py Blanks

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,12 +30,13 @@ def teardown_appcontext(self):
 @app.errorhandler(404)
 def error_404(error):
     """
-    handler for 404 errors that returns a JSON-formatted 404 status code 
+    handler for 404 errors that returns a JSON-formatted 404 status code
     response. The content should be: "error": "Not found"
     """
 
     error = {"error": "Not found"}
     return make_response(jsonify(error), 404)
+
 
 if __name__ == "__main__":
     host = get("HBNB_API_HOST", "0.0.0.0")
