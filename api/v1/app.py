7ded73418cd2fff579c04076c91a5806b91cfee7
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-08 19:33:51 -0500

    adding 404 error function with decorator errorhandler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,8 @@
 
 
 from flask import Flask
+from flask import make_response
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv as get
@@ -24,6 +26,16 @@ def teardown_appcontext(self):
     """
     storage.close()
 
+
+@app.errorhandler(404)
+def error_404(error):
+    """
+    handler for 404 errors that returns a JSON-formatted 404 status code 
+    response. The content should be: "error": "Not found"
+    """
+
+    error = {"error": "Not found"}
+    return make_response(jsonify(error), 404)
 
 if __name__ == "__main__":
     host = get("HBNB_API_HOST", "0.0.0.0")
