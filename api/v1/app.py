ab691363aed2cc40f44286b0bc833c202283e871
FranQ <frankchristopherquispe@gmail.com>
2022-12-10 06:20:03 +0000

    app.py <>task13

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from flask_cors import CORS
 
 # create a variable appp, instance of flask
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 # register the blueprint app_views to your instance app
 app.register_blueprint(app_views)
